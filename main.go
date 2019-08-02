19e5c7e2f370b69427673e3405e44dc28d4c0ab9
arifken <arifken@zillowgroup.com>
2019-08-02 10:09:44 -0700

    Initial implementation of step

--- main.go
+++ main.go
@@ -1,31 +1,112 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
-	"os/exec"
+
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-steputils/stepconf"
 )
 
-func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+type config struct {
+	PersonalAccessToken string `env:"bitbucket_token,required"`
+	CommitHash          string `env:"commit_hash,required"`
+	APIBaseURL          string `env:"api_base_url,required"`
+	BuildKey            string `env:"build_key,required"`
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	Status      string `env:"status,opt[auto,in_progress,successful,failed]"`
+	TargetURL   string `env:"target_url"`
+	BuildName   string `env:"build_name"`
+	Description string `env:"description"`
+}
+
+func getState(status string) string {
+	switch status {
+	case "in_progress":
+		return "INPROGRESS"
+	case "successful":
+		return "SUCCESSFUL"
+	case "failed":
+		return "FAILED"
+	}
+
+	if os.Getenv("BITRISE_BUILD_STATUS") == "0" {
+		return "successful"
+	}
+	return "failed"
+}
+
+func getName(cfg config) string {
+	if cfg.BuildName == "" {
+		return cfg.BuildKey
+	}
+
+	return cfg.BuildName
+}
+
+func getAPIURL(cfg config) string {
+	return fmt.Sprintf("%s/build-status/1.0/commits/%s", cfg.APIBaseURL, cfg.CommitHash)
+}
+
+func postStatus(cfg config) error {
+	params := map[string]string{
+		"state":       getState(cfg.Status),
+		"key":         cfg.BuildKey,
+		"name":        getName(cfg),
+		"url":         cfg.TargetURL,
+		"description": cfg.Description,
+	}
+
+	jsonParams, err := json.Marshal(params)
 	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+		return err
+	}
+
+	url := getAPIURL(cfg)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonParams))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", cfg.PersonalAccessToken))
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send the request: %s", err)
+	}
+
+	if err := response.Body.Close(); err != nil {
+		return err
+	}
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		return fmt.Errorf("server error: %s", response.Status)
+	}
+
+	return err
+}
+
+func main() {
+	if os.Getenv("commit_hash") == "" {
+		log.Errorf("Cannot publish a build status without a commit")
 		os.Exit(1)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		log.Errorf("Error parsing configuration: %s\n", err)
+		os.Exit(1)
+	}
+	stepconf.Print(cfg)
+
+	if err := postStatus(cfg); err != nil {
+		log.Errorf("Error posting build status: %s\n", err)
+		os.Exit(1)
+	}
+
 	os.Exit(0)
 }
